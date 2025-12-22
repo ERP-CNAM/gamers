9c8e759ce9e6f232658896b7ca7f80b9a9a54c44
Léo HOTZ <cdf.leo.hotz@gmail.com>
2025-12-22 16:20:18 +0100

    Feat: Add hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Header } from '../components/header/header';
+import { Home } from '../home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
