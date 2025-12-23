b1b39083806184514f3ba7ecb51ec3c108ded080
Baptiste <baptiste.bdry@gmail.com>
2025-12-23 11:35:30 +0100

    Feat : add profil page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Loginpage } from './loginpage/loginpage';
 import { Homepage } from './homepage/homepage';
+import { Profilepage } from './profilepage/profilepage';
 
 export const routes: Routes = [
   { path: '', component: Homepage },
   { path: 'login', component: Loginpage },
+  { path: 'profil', component: Profilepage },
 ];
