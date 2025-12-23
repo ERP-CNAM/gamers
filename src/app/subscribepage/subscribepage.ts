42dd2dbdca5192c17a0f3f497ec2f67624263546
ShortLegsFox <mister.dusbin777@gmail.com>
2025-12-23 11:03:01 +0100

    creating subscribe page

---
+++ src/app/subscribepage/subscribepage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribepage',
+  imports: [],
+  templateUrl: './subscribepage.html',
+  styleUrl: './subscribepage.css',
+})
+export class Subscribepage {
+
+}
