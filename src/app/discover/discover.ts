4cfca81c7c7d3132c8fcc8ca552a2e9f3a21f893
Maxime BLAESS <blaessmaxime@gmail.com>
2026-01-20 10:23:35 +0100

    Correction

--- src/app/discover/discover.ts
+++ src/app/discover/discover.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-discover',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './discover.html'
 })
 export class DiscoverPage {}
