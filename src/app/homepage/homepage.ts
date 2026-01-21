130cee543fab97334ec4cfa92524689fcb7cf1f0
Léo HOTZ <cdf.leo.hotz@gmail.com>
2026-01-21 22:21:37 +0100

    Style: Bring contact us page to main page

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../components/hero/hero';
 import { DiscoverPage } from '../discover/discover';
+import { Contactuspage } from '../contactuspage/contactuspage';
 
 @Component({
   selector: 'app-homepage',
-  imports: [Hero, DiscoverPage],
+  imports: [Hero, DiscoverPage, Contactuspage],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
